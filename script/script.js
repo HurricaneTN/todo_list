0b409e1b2879542638c046a055d58d1abdb32de9
Houssem Rouabeh <81163379+HurricaneTN@users.noreply.github.com>
2023-01-04 15:35:53 +0100

    localstorage v1

--- script/script.js
+++ script/script.js
@@ -1,30 +1,70 @@
 const buttonClicked = document.getElementById("btn");
+const clearClicked = document.getElementById("clr");
 const input = document.getElementById("input");
 const container = document.getElementsByClassName("container");
+var isDone = false;
 
-buttonClicked.addEventListener("click", function () {
-  const child = document.createElement("h3");
-  if (input.value == "") {
-    alert("Entrer une tache d'abord");
+buttonClicked.addEventListener("click", addTask);
+clearClicked.addEventListener("click", clearTasks);
+
+function addTask() {
+  if (input.value === "") {
+    alert("Enter task before !! ");
   } else {
-    const content = document.createTextNode(input.value);
-    child.appendChild(content);
-    container[0].appendChild(child);
+    const tasksSet = JSON.parse(localStorage.getItem("tasks"));
+    if (tasksSet === null) {
+      taskList = [];
+    } else {
+      taskList = tasksSet;
+    }
+    taskList.push(input.value);
+    localStorage.setItem("tasks", JSON.stringify(taskList));
     input.value = "";
-    var isDone = false;
-    child.addEventListener("click", function () {
-      if (isDone == true) {
-        child.classList.remove("done");
-        isDone = false;
-      } else {
-        child.classList.add("done");
-        isDone = true;
-      }
-    });
-    child.addEventListener("dblclick", function () {
-      if (confirm("Voulez vous vraiment supprimer cette tâche ?")) {
-        container[0].removeChild(child);
-      }
-    });
+
+    loadTasks();
   }
-});
+}
+function loadTasks() {
+  let output = "";
+  const tasksSet = JSON.parse(localStorage.getItem("tasks"));
+
+  if (tasksSet === null) {
+    taskList = [];
+  } else {
+    taskList = tasksSet;
+  }
+
+  taskList.forEach((data, index) => {
+    output += `
+      <div class="tasksList">
+        <h3 onclick="taskDone(${index})" id="${index}">${data}</h3>
+        <button onclick="removeTask(${index})">X</button>
+      </div>
+    `;
+  });
+
+  container[0].innerHTML = output;
+}
+function taskDone(index) {
+  const toBeDone = document.getElementById(index);
+  if (isDone == true) {
+    toBeDone.classList.remove("done");
+    isDone = false;
+  } else {
+    toBeDone.classList.add("done");
+    isDone = true;
+  }
+}
+function removeTask(index) {
+  if (confirm("Are you sure to delete task ?")) {
+    const tasksSet = JSON.parse(localStorage.getItem("tasks"));
+    tasksSet.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasksSet));
+    loadTasks();
+  }
+}
+function clearTasks() {
+  localStorage.clear();
+  loadTasks();
+}
+loadTasks();
